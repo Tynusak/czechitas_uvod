7e93f23c2443ce24d409bba24c22f1a7b1923937
Tynusak <kristyna.svandova@gmail.com>
2022-01-22 14:27:27 +0100

    1 file modified

--- ukol4.js
+++ ukol4.js
@@ -1,8 +1,8 @@
 let i = Number (prompt ('Zadej číslo:'));
 let x = 0;
 while (i != 0){
-  x = x+ i;
-  í = Number (prompt ('Zadej číslo:'));
+  x = x + i;
+  i = Number (prompt ('Zadej číslo:'));
   console.log('součet je:'+i);
 }
 
