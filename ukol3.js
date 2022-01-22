f9d194fd36e35ee75e57161bdf70d9b9a90ce93d
Tynusak <kristyna.svandova@gmail.com>
2022-01-22 13:05:45 +0100

    1 file modified

--- ukol3.js
+++ ukol3.js
@@ -2,7 +2,10 @@ let jmeno = prompt ('Jak se jmenujete?');
 let vek = Number (prompt ('Kolik je vám let?'));
 
 
-if(vek < 18)
-  console.log('Vaše jméno je '+jmeno+',je vám '+vek+' let a můžete řídit auto.') 
+if(vek < 18) {
+  console.log('Vaše jméno je '+jmeno+',je vám '+vek+' let a musíte si počkat ještě '+(18-vek)+' roky než budete moci řídit auto.') }
+  else {
+    console.log('Vaše jméno je '+jmeno+',je vám '+vek+' let a můžete řídit auto.')
+  }
 
 
